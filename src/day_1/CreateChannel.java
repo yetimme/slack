6da3ffdbe56af77a5786b29e3c7d23656aa1ec89
yetimme <yetimme@yahoo.com>
2020-05-12 17:19:58 +0100

    modify Create Channel

--- src/day_1/CreateChannel.java
+++ src/day_1/CreateChannel.java
@@ -5,5 +5,7 @@ public class CreateChannel {
     public static void main(String[] args) {
 
         System.out.println("User is able to create channels");
+
+        System.out.println("Something added");
     }
 }
